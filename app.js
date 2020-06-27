23a7dfd5491f66a94f749fb765f0d788771377e5
tushar1999sharma <tushar1999sharma@gmail.com>
2020-06-28 02:44:22 +0530

    add basic backend

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express       = require("express");
 const app           = express();
+const session       = require('express-session');
+const mongoose      = require('mongoose');
+
 const routes        = require("./src/backEnd/routes") //it will automatically take index name file
 const PORT          = process.env.PORT || "5000";
 
-app.use(express.json()); //parse body
-app.use(express.urlencoded({ extended: false }));
-require("./src/backEnd/routes")(router);
+//connect with Mongo DB
+mongoose.connect('mongodb://localhost:27017/TopTidings', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
 
+//create session
+app.use(session(
+    {
+        name: 'TopTidings',
+        secret: 'mySecret',
+        saveUninitialized: false,
+        resave: false,
+    }
+));
+
+app.use(express.json()); //parse json
+app.use(express.urlencoded({ extended: false }));
+
+//use routes
 app.use('/', routes);
 
 //Listen SERVER
-app.listen(PORT, (error)=>
+app.listen(PORT, (error) =>
 {
     if(error)
     {
